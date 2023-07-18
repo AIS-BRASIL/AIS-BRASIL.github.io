536cd5ed30ad30ae409df2773e24c171570ec6a0
Rodrigo C. Arruda <rodrigo.arruda@aluno.cefet-rj.br>
2023-07-18 12:02:03 -0300

    bug fix desktop v0.6.1

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,22 +1,25 @@
 window.addEventListener('scroll', function() {
-	var bannerHeight = document.getElementById('banner').offsetHeight;
-	var ctaHeight = document.getElementById('cta').offsetHeight;
-	var scrollPosition = window.scrollY;
+	if(window.innerWidth <= 720)
+	{
+		var bannerHeight = document.getElementById('banner').offsetHeight;
+		var ctaHeight = document.getElementById('cta').offsetHeight;
+		var scrollPosition = window.scrollY;
 
-	if (scrollPosition > bannerHeight / 3) {
-		document.getElementById('header').style.display = 'none';
-	}
-	 else {
-		document.getElementById('header').style.display = 'flex';
-	}
+		if (scrollPosition > bannerHeight / 3) {
+			document.getElementById('header').style.display = 'none';
+		}
+		 else {
+			document.getElementById('header').style.display = 'flex';
+		}
 
-	if(scrollPosition > ctaHeight / 3){
-		document.getElementById('homebuttons').style.display = 'block';
-		if(scrollPosition > bannerHeight){
+		if(scrollPosition > ctaHeight / 3){
+			document.getElementById('homebuttons').style.display = 'block';
+			if(scrollPosition > bannerHeight){
+				document.getElementById('homebuttons').style.display = 'none';
+			}
+		}
+		else{
 			document.getElementById('homebuttons').style.display = 'none';
 		}
-	}
-	else{
-		document.getElementById('homebuttons').style.display = 'none';
 	}
 });
